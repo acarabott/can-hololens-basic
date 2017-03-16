c7eea27eb20721376209102af22586089b9b5fbc
Arthur Carabott <arthurc@gmail.com>
2017-03-16 16:32:07 +0000

    remove logging

--- Assets/Scripts/WorldCursor.cs
+++ Assets/Scripts/WorldCursor.cs
@@ -7,7 +7,6 @@ public class WorldCursor : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Debug.Log("Cursor start");
         // Grab the mesh renderer that's on the same object as this script.
         meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
     }
