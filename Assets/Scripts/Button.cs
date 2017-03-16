c1cf7a65680dd99ad4fb532e5378c4c15c60567c
Arthur Carabott <arthurc@gmail.com>
2017-03-16 12:24:31 +0000

    buttons ready for testing

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+  public string buttonValue;
+  public GameObject textObj;
+
+  // Use this for initialization
+  void Start () {
+    textObj = this.transform.Find("cube text").gameObject;
+    if (textObj != null) {
+      TextMesh textMesh = textObj.GetComponent( typeof(TextMesh) ) as TextMesh;
+      textMesh.text = buttonValue;
+    }
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  // Called by GazeGestureManager when the user performs a Select gesture
+  void OnSelect()
+  {
+    Debug.Log(buttonValue);
+  }
+}
