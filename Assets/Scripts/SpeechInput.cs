8e2c32a8d80c9e04a212729718ac32ad94ee0329
Arthur Carabott <arthurc@gmail.com>
2017-03-18 17:24:01 +0000

    attempt speech input test

---
+++ Assets/Scripts/SpeechInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class SpeechInput : MonoBehaviour, ISpeechHandler {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData) {
+    Debug.Log(eventData.RecognizedText.ToLower());
+    if (GazeGestureManager.Instance.FocusedObject == this.gameObject) {
+      Debug.Log("looking good");
+    }
+  }
+}
