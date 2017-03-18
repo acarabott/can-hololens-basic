2df2ae1dba573c480c1fd4e4e3be9b35d5548c5b
Arthur Carabott <arthurc@gmail.com>
2017-03-18 13:14:47 +0000

    starting expansion of clock

---
+++ Assets/Scripts/ClockButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var angles = transform.eulerAngles;
+
+    var position = transform.localPosition;
+    // Debug.LogFormat("this: {0}", position);
+    // var radius = transform.localScale;
+    // var radius = 1.0f / transform.localScale.x;
+    var radius = 0.09f;
+    var x = Mathf.Cos((Mathf.PI / 180.0f) * (angles.z + 90.0f)) * radius;
+    var y = Mathf.Sin((Mathf.PI / 180.0f) * (angles.z + 90.0f)) * radius;
+
+    position.x = x;
+    position.y = y;
+    // transform.localPosition = position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
