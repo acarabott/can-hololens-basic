93e11ea7638846d71953fdcfe3ea1b50487f3241
acarabott <arthurc@gmail.com>
2017-03-20 00:07:00 +0000

    indentation

--- Assets/Scripts/SpeechInput.cs
+++ Assets/Scripts/SpeechInput.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
 public class SpeechInput : MonoBehaviour, ISpeechHandler {
-  Dictionary<string, string> lookup = new Dictionary<string, string>();
+    Dictionary<string, string> lookup = new Dictionary<string, string>();
 
 	// Use this for initialization
 	void Start () {
-    lookup.Add("zero", "0");
-    lookup.Add("o", "0");
-    lookup.Add("oh", "0");
-    lookup.Add("one", "1");
-    lookup.Add("two", "2");
-    lookup.Add("three", "3");
-    lookup.Add("four", "4");
-    lookup.Add("five", "5");
-    lookup.Add("six", "6");
-    lookup.Add("seven", "7");
-    lookup.Add("eight", "8");
-    lookup.Add("nine", "9");
-    lookup.Add("dot", ".");
-    lookup.Add("point", ".");
-    lookup.Add("decimal", ".");
-    lookup.Add("negative", "-");
-    lookup.Add("minus", "-");
-    lookup.Add("positive", "-");
-    lookup.Add("clear", "clear");
+        lookup.Add("zero", "0");
+        lookup.Add("o", "0");
+        lookup.Add("oh", "0");
+        lookup.Add("one", "1");
+        lookup.Add("two", "2");
+        lookup.Add("three", "3");
+        lookup.Add("four", "4");
+        lookup.Add("five", "5");
+        lookup.Add("six", "6");
+        lookup.Add("seven", "7");
+        lookup.Add("eight", "8");
+        lookup.Add("nine", "9");
+        lookup.Add("dot", ".");
+        lookup.Add("point", ".");
+        lookup.Add("decimal", ".");
+        lookup.Add("negative", "-");
+        lookup.Add("minus", "-");
+        lookup.Add("positive", "-");
+        lookup.Add("clear", "clear");
 	}
 
 	// Update is called once per frame
@@ -34,12 +34,12 @@ public class SpeechInput : MonoBehaviour, ISpeechHandler {
 
 	}
 
-  public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData) {
-    var text = eventData.RecognizedText.ToLower();
-
-    if (lookup.ContainsKey(text))
-    {
-      this.transform.SendMessageUpwards("OnInput", lookup);
+    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData) {
+        var text = eventData.RecognizedText.ToLower();
+        Debug.LogFormat("Recognized: {0}", text);
+        if (lookup.ContainsKey(text)) {
+            Debug.Log("key found");
+            this.transform.SendMessageUpwards("OnInput", lookup[text]);
+        }
     }
-  }
 }
